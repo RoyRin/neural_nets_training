8d884119a6e75c83703771a09ce0489ade335ca2
Roy Rinberg <royrinberg@gmail.com>
2022-02-14 12:39:51 -0500

    MNIST - > mnist naming

--- neural_nets_training/dataset_management.py
+++ neural_nets_training/dataset_management.py
@@ -8,17 +8,19 @@ from neural_nets_training import params
 
 
 def load_dataloader(dataset, batch_size=params.batch_size):
+    """ Function to create data loaders from dataset """
     return torch.utils.data.DataLoader(dataset,
                                        batch_size=batch_size,
                                        shuffle=False,
                                        num_workers=2)
 
 
-def get_CIFAR10_dataset(dirpath="./data/CIFAR10"):
+def get_cifar10_dataset(dirpath="./data/CIFAR10"):
     """
+    Get CIFAR 10 dataset
     Returns:
         [Tuple]: 
-            (trainset, testset), (trainloader, testloader)
+            (trainset, testset)
     """
     CIFAR10_MEAN = (0.4914, 0.4822, 0.4465)
     CIFAR10_STD_DEV = (0.2023, 0.1994, 0.2010)
@@ -50,14 +52,14 @@ def get_CIFAR10_dataset(dirpath="./data/CIFAR10"):
     return trainset, testset
 
 
-def load_CIFAR10_datasets_and_loaders(dirpath="./data/CIFAR10",
+def load_cifar10_datasets_and_loaders(dirpath="./data/CIFAR10",
                                       batch_size=params.batch_size):
     """
     Returns:
         [Tuple]: 
             (trainset, testset), (trainloader, testloader)
     """
-    trainset, testset = get_CIFAR10_dataset(dirpath)
+    trainset, testset = get_cifar10_dataset(dirpath)
     trainloader, testloader = load_dataloader(
         trainset,
         batch_size=batch_size), load_dataloader(testset, batch_size=batch_size)
@@ -65,7 +67,54 @@ def load_CIFAR10_datasets_and_loaders(dirpath="./data/CIFAR10",
     return (trainset, testset), (trainloader, testloader)
 
 
-def get_MNIST_dataset(dirpath="./data/MNIST"):
+def get_cifar100_dataset(dirpath="./data/CIFAR100", transforms=None):
+    """Get CIFAR 100 dataset
+
+    Args:
+        dirpath (str, optional): _description_. Defaults to "./data/cifar100".
+        transforms (_type_, optional): _description_. Defaults to None.
+    """
+    if transforms is None:
+        transforms = torchvision.transforms.Compose([
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize((0.5, ), (0.5, ), inplace=True),
+        ])
+    cifar100_train = torchvision.datasets.CIFAR100("./data/cifar100",
+                                                   train=True,
+                                                   download=True,
+                                                   transform=transforms)
+    cifar100_test = torchvision.datasets.CIFAR100("./data/cifar100",
+                                                  train=False,
+                                                  download=True,
+                                                  transform=transforms)
+    return cifar100_train, cifar100_test
+
+
+def load_cifar100_datasets_and_loaders(dirpath="./data/CIFAR100",
+                                       batch_size=params.batch_size,
+                                       transforms=None):
+    """
+    Returns:
+        [Tuple]: 
+            (trainset, testset), (trainloader, testloader)
+    """
+    trainset, testset = get_cifar100_dataset(dirpath, transforms=transforms)
+    trainloader, testloader = load_dataloader(
+        trainset,
+        batch_size=batch_size), load_dataloader(testset, batch_size=batch_size)
+
+    return (trainset, testset), (trainloader, testloader)
+
+
+def get_mnist_dataset(dirpath="./data/MNIST"):
+    """Get MNIST dataset
+
+    Args:
+        dirpath (str, optional): _description_. Defaults to "./data/MNIST".
+
+    Returns:
+        tuple : (trainset, testset)
+    """
     transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.5, ), (0.5, ), inplace=True),
@@ -82,7 +131,7 @@ def get_MNIST_dataset(dirpath="./data/MNIST"):
     return trainset, testset
 
 
-def load_MNIST_datasets_and_loaders(dirpath="./data/MNIST",
+def load_mnist_datasets_and_loaders(dirpath="./data/MNIST",
                                     batch_size=params.batch_size):
     """
     Returns:
@@ -90,7 +139,7 @@ def load_MNIST_datasets_and_loaders(dirpath="./data/MNIST",
             (trainset, testset), (trainloader, testloader)
     """
 
-    trainset, testset = get_MNIST_dataset(dirpath)
+    trainset, testset = get_mnist_dataset(dirpath)
     trainloader, testloader = load_dataloader(
         trainset,
         batch_size=batch_size), load_dataloader(testset, batch_size=batch_size)
@@ -109,18 +158,21 @@ def get_data_loaders(data_loader, num_teachers, batch_size=params.batch_size):
                                     batch_size=batch_size)
         for teacher in range(num_teachers)
     ]
-    """
-    teacher_loaders = []
-    for i in range(data_size):
-        indices = list(range(i*data_size, (i+1)*data_size))
-        subset_data = Subset(data_loader, indices)
-        loader = torch.utils.data.DataLoader(subset_data, batch_size=batch_size)
-        teacher_loaders.append(loader)
-    return teacher_loaders
-    """
 
 
 def dataset_from_indices(mask, dataset):
+    """Generate a new dataset from a mask and a dataset.
+
+    Args:
+        mask (_type_): _description_
+        dataset (_type_): _description_
+
+    Raises:
+        Exception: _description_
+
+    Returns:
+        dataset
+    """
     indices = mask.nonzero()[0]
     if not (isinstance(indices, np.ndarray)):
         raise Exception(f"Indices are not numpy array: {type(indices)}")
@@ -128,6 +180,7 @@ def dataset_from_indices(mask, dataset):
 
 
 def get_uneven_data_loaders(dataset, lengths):
+    """ Function to create an uneven collection of dataloaders"""
     N = len(dataset)
     percent = int(N / 100)
     lengths = [
