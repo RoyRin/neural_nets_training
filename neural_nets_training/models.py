55b47be928071de2b087cfdfbdf2f0b5844a6744
Roy Rinberg <royrinberg@gmail.com>
2022-02-16 00:21:56 -0500

    try out resnet 50

--- neural_nets_training/models.py
+++ neural_nets_training/models.py
@@ -92,6 +92,7 @@ class memorization_FCN(nn.Module):
 
 # CNN Model
 class CNN(nn.Module):
+    """2 layer CNN"""
     def __init__(self, num_classes=10):
         super(CNN, self).__init__()  # super(Net, self).__init__()
         self.basemodel_name = "CNN"
@@ -104,6 +105,7 @@ class CNN(nn.Module):
 
     # x represents our data
     def forward(self, x):
+        """forward inference"""
         # yapf: disable
         return toolz.pipe(x, self.conv1, F.relu,
                             self.conv2, F.relu,
@@ -115,6 +117,135 @@ class CNN(nn.Module):
                             self.fc2,
                             lambda x: F.log_softmax(x, dim=1))
         # yapf: enable
+
+
+# code taken from https://github.com/weiaicunzai/pytorch-cifar100/blob/master/models/resnet.py
+class BottleNeck(nn.Module):
+    """Residual block for resnet over 50 layers"""
+    expansion = 4
+
+    def __init__(self, in_channels, out_channels, stride=1):
+        super().__init__()
+        self.residual_function = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels,
+                      out_channels,
+                      stride=stride,
+                      kernel_size=3,
+                      padding=1,
+                      bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels,
+                      out_channels * BottleNeck.expansion,
+                      kernel_size=1,
+                      bias=False),
+            nn.BatchNorm2d(out_channels * BottleNeck.expansion),
+        )
+
+        self.shortcut = nn.Sequential()
+
+        if stride != 1 or in_channels != out_channels * BottleNeck.expansion:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels,
+                          out_channels * BottleNeck.expansion,
+                          stride=stride,
+                          kernel_size=1,
+                          bias=False),
+                nn.BatchNorm2d(out_channels * BottleNeck.expansion))
+
+    def forward(self, x):
+        """forward inference"""
+        return nn.ReLU(inplace=True)(self.residual_function(x) +
+                                     self.shortcut(x))
+
+
+class ResNet(nn.Module):
+    """ Large Resnet base class"""
+    def __init__(self, block, num_block, num_classes=100):
+        super().__init__()
+
+        self.in_channels = 64
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(64), nn.ReLU(inplace=True))
+        #we use a different inputsize than the original paper
+        #so conv2_x's stride is 1
+        self.conv2_x = self._make_layer(block, 64, num_block[0], 1)
+        self.conv3_x = self._make_layer(block, 128, num_block[1], 2)
+        self.conv4_x = self._make_layer(block, 256, num_block[2], 2)
+        self.conv5_x = self._make_layer(block, 512, num_block[3], 2)
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+    def _make_layer(self, block, out_channels, num_blocks, stride):
+        """make resnet layers(by layer i didnt mean this 'layer' was the
+        same as a neuron netowork layer, ex. conv layer), one layer may
+        contain more than one residual block
+        Args:
+            block: block type, basic block or bottle neck block
+            out_channels: output depth channel number of this layer
+            num_blocks: how many blocks per layer
+            stride: the stride of the first block of this layer
+        Return:
+            return a resnet layer
+        """
+
+        # we have num_block blocks per layer, the first block
+        # could be 1 or 2, other blocks would always be 1
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_channels, out_channels, stride))
+            self.in_channels = out_channels * block.expansion
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        """ forward inference for resnet"""
+        output = self.conv1(x)
+        output = self.conv2_x(output)
+        output = self.conv3_x(output)
+        output = self.conv4_x(output)
+        output = self.conv5_x(output)
+        output = self.avg_pool(output)
+        output = output.view(output.size(0), -1)
+        output = self.fc(output)
+
+        return output
+
+
+def Resnet50_model_factory(*,
+                           num_classes=10,
+                           device=params.get_default_device()):
+    """ return a ResNet 50 object
+    """
+    model = ResNet(BottleNeck, [3, 4, 6, 3], num_classes=num_classes)
+    model.to(device)
+    return model
+
+
+def Resnet101_model_factory(*,
+                            num_classes=10,
+                            device=params.get_default_device()):
+    """ return a ResNet 101 object
+    """
+    model = ResNet(BottleNeck, [3, 4, 23, 3], num_classes=num_classes)
+    model.to(device)
+    return model
+
+
+def Resnet152_model_factory(*,
+                            num_classes=10,
+                            device=params.get_default_device()):
+    """ return a ResNet 152 object
+    """
+    model = ResNet(BottleNeck, [3, 8, 36, 3], num_classes=num_classes)
+    model.to(device)
+    return model
 
 
 def parameters_per_model(model):
